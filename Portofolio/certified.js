ade84c65ffc83c487e618dc800e132434e2f5119
ivannurbahtiar <networkingxi@gmail.com>
2025-10-09 14:14:07 +0700

    Kamis

--- Portofolio/certified.js
+++ Portofolio/certified.js
@@ -19,3 +19,31 @@ tabButtons.forEach((btn) => {
     });
   });
 });
+
+// Ambil elemen modal
+const modal = document.getElementById("imageModal");
+const modalImg = document.getElementById("modalImage");
+const closeBtn = document.querySelector(".close");
+
+// Semua gambar sertifikat
+const certImages = document.querySelectorAll(".certificate-card img");
+
+// Saat gambar diklik
+certImages.forEach((img) => {
+  img.addEventListener("click", () => {
+    modal.style.display = "flex";
+    modalImg.src = img.src;
+  });
+});
+
+// Tombol close ditekan
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+// Klik di luar gambar menutup modal
+modal.addEventListener("click", (e) => {
+  if (e.target === modal) {
+    modal.style.display = "none";
+  }
+});
